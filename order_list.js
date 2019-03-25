1adb6c7623c853a01ae5f1bc75ffd55ae94bb1f8
ivishalydv <vishalydv.me@gmail.com>
2019-03-25 08:35:37 +0530

    v0.2

---
+++ order_list.js
@@ -0,0 +1,10 @@
+var green_salad=document.getElementById("green_salad");
+var count_green_salad=0;
+green_salad.addEventListener("click",function{
+    count_green_salad++;
+});
+if(count_green_salad>0)
+{
+    var li=document.getElementsByTagName(li);
+
+}
