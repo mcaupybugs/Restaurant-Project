b374738d63835c6c91d01dbe543c08cd1250efb6
ivishalydv <vishalydv.me@gmail.com>
2019-04-07 15:40:24 +0530

    added functional + button

--- order.js
+++ order.js
@@ -1,7 +1,8 @@
-var green_count=document.getElementById('green_count');
-function incnumber(){
-    var count=green_count.innerHTML;
+//function to increase the count of green salad
+function incnumber(value){   //Value is equal to the id of span tag
+    var greenn=document.getElementById(value);//Accessing the span tag using value attribute
+    var count=greenn.innerHTML;
     count++;
-    green_count.innerHTML=count;
+    greenn.innerHTML=count;
     return true;
 };
