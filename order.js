d739ddc98fe5a58ffb02bd0da8e3df7b3249589d
ivishalydv <vishalydv.me@gmail.com>
2019-04-07 16:02:45 +0530

    cart functional v0.5

--- order.js
+++ order.js
@@ -1,8 +1,19 @@
 //function to increase the count of green salad
 function incnumber(value){   //Value is equal to the id of span tag
-    var greenn=document.getElementById(value);//Accessing the span tag using value attribute
-    var count=greenn.innerHTML;
+    var item=document.getElementById(value);//Accessing the span tag using value attribute
+    var count=item.innerHTML;
     count++;
-    greenn.innerHTML=count;
+    item.innerHTML=count;
+    return true;
+};
+
+// Function to decrease the value of cart
+function decnumber(value){
+    var item=document.getElementById(value);//Accessing the span tag using value attribute
+    var count=item.innerHTML;
+    if(count>0){
+        count--;
+    }
+    item.innerHTML=count;
     return true;
 };
