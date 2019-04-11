f98e7e353ffc5ccc6b3b11fb81d0f42b50e5f848
ivishalydv <vishalydv.me@gmail.com>
2019-04-12 04:35:22 +0530

    back-end added v0.6

---
+++ app.js
@@ -0,0 +1,43 @@
+var express=require('express');
+var app=express();
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/',(req,res)=>
+{
+    res.render("../index");
+    console.log("homepage logged");
+});
+
+app.get('/about',(req,res)=>
+{
+    res.render("../about");
+    console.log("About section logged");
+});
+
+app.get('/gallery',(req,res)=>
+{
+    res.render("../gallery");
+    console.log("Gallery logged");
+});
+
+app.get('/menu',(req,res)=>{
+    res.render("../menu");
+    console.log("Menu logged");
+});
+
+app.get('/cart',(req,res)=>{
+    res.render("../order_online");
+    console.log("Cart logged");
+});
+
+const port=process.env.PORT || 3000;
+
+app.listen(port,()=>
+{
+    console.log(`listening on port ${port}`);
+});
+
